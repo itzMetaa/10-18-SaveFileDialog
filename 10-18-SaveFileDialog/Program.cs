2cca330e037926aeb98a4decff4f5eabc5ff1abf
me <you@example.com>
2019-10-18 10:35:09 +0200

    initialize

--- 10-18-SaveFileDialog/Program.cs
+++ 10-18-SaveFileDialog/Program.cs
@@ -16,7 +16,7 @@ namespace _10_18_SaveFileDialog
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new formMain());
         }
     }
 }
