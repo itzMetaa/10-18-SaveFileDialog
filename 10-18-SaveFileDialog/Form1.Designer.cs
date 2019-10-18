cc9ee4e23a577005e6c7df1433e0897900a13fbb
me <you@example.com>
2019-10-18 10:40:02 +0200

    nevek

--- 10-18-SaveFileDialog/Form1.Designer.cs
+++ 10-18-SaveFileDialog/Form1.Designer.cs
@@ -28,22 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBoxKiir = new System.Windows.Forms.ListBox();
+            this.textBoxBevitel = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.SuspendLayout();
+            // 
+            // listBoxKiir
+            // 
+            this.listBoxKiir.FormattingEnabled = true;
+            this.listBoxKiir.Location = new System.Drawing.Point(147, 35);
+            this.listBoxKiir.Name = "listBoxKiir";
+            this.listBoxKiir.Size = new System.Drawing.Size(143, 238);
+            this.listBoxKiir.TabIndex = 0;
+            // 
+            // textBoxBevitel
+            // 
+            this.textBoxBevitel.Location = new System.Drawing.Point(27, 35);
+            this.textBoxBevitel.Name = "textBoxBevitel";
+            this.textBoxBevitel.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBevitel.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(27, 61);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Mentés";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(27, 90);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Betöltés";
+            this.btnLoad.UseVisualStyleBackColor = true;
             // 
             // formMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(302, 390);
+            this.ClientSize = new System.Drawing.Size(302, 287);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.textBoxBevitel);
+            this.Controls.Add(this.listBoxKiir);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "formMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Program";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxKiir;
+        private System.Windows.Forms.TextBox textBoxBevitel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
